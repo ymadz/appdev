4bfa0ce157eac197079f07f6835474a80dbafa5b
ymadz <138951434+ymadz@users.noreply.github.com>
2025-02-04 16:40:39 +0800

    with html files

--- crud/crud/views.py
+++ crud/crud/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def crud(request):
+    return render(request, 'crud.html')
