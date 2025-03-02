2186e36a08479b2d2cb652eb8b3a9e69cebd2a5a
ymadz <138951434+ymadz@users.noreply.github.com>
2025-03-02 11:46:49 +0800

    mysql integration

--- mycrud/myproject/settings.py
+++ mycrud/myproject/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'NAME': 'pracprac',
     }
 }
 
