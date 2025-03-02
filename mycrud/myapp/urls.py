8a0e252c56da45c1673e5f6079f7c3db64f8c7e5
ymadz <138951434+ymadz@users.noreply.github.com>
2025-03-02 13:53:02 +0800

    crud operation

--- mycrud/myapp/urls.py
+++ mycrud/myapp/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    
+    path('', views.product_list, name='product_list'),
+    path('new/', views.product_create, name='product_create'),
+    path('<int:pk>/edit/', views.product_update, name='product_update'),
+    path('<int:pk>/delete/', views.product_delete, name='product_delete'),
 ]
