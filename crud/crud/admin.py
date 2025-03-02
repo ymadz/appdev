14ff338b45395aabe207f84018fea3981c457404
ymadz <138951434+ymadz@users.noreply.github.com>
2025-03-02 11:39:16 +0800

    initial django setup

--- crud/crud/admin.py
+++ crud/crud/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'age')
+    
+admin.site.register(Student, StudentAdmin)
+admin.site.register(Course)
