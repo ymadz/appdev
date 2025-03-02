758ecbe5b52d704c850df828cdd442cd560ad282
ymadz <138951434+ymadz@users.noreply.github.com>
2025-03-03 05:41:58 +0800

    created superuser

--- crudpractice/myapp/admin.py
+++ crudpractice/myapp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Author, Book
 
 # Register your models here.
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author']
+    
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Book, BookAdmin)
+
